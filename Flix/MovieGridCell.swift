7785ffdc2002925ed33e63e1e89892f95f66bb0a
Kmendoza2018 <kmendoza21981@hotmail.com>
2022-09-13 23:04:13 -0400

    Updated Readme File with Part 2 of Flix as well as code

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Kyle Mendoza on 9/13/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+}
